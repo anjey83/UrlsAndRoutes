a3c4c5b7180b191ab13ed794b99a6b8340c49cbf
Anjey Brusensky <anjey.brusensky@gmail.com>
2019-09-26 14:29:12 +0200

    using custom variables as action method parameters

--- UrlsAndRoutes/Controllers/HomeController.cs
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -15,11 +15,14 @@ namespace UrlsAndRoutes.Controllers
             Action = nameof( Index )
         } );
 
-        public ViewResult CustomVariable( )
+        //value from third segment is assigned to the custom variable 'id'
+        //MVC compares the list of segment variables with the list of action method parameters and, if the names match, passes the values from the URL to the method
+        //parameter automatically converts to the defined type in action
+        public ViewResult CustomVariable( string id)
         {
             Result r = new Result { Controller = nameof( HomeController ),
                 Action = nameof( CustomVariable ), };
-            r.Data["Id"] = RouteData.Values["id"];
+            r.Data["Id"] = id;
 
             return View( "Result", r ); }
     }
